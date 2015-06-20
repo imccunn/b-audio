152a58e5ca0fbd6041fb8271848ce61575e3c303
Ian McCunn <ianmccunn@gmail.com>
2015-06-20 15:37:09 -0700

    a change

--- 101_test.js
+++ 101_test.js
@@ -3,21 +3,21 @@
 var baudio = require('baudio');
 var tau = 2 * Math.PI;
 
-var melody = [0, 5/4, 12/11, -3/2, 0, -1/3, 5/4, 5/2]
+var melody = [0, 5/4, 3/4, 12/11, -3/2, 0, -1/3, 5/4, 5/2]
               .map(function(x) { return Math.pow(2, x)});
 
 var b = baudio(function(t) {
   var f = 100 + (t % 5 === 0 ?  Math.sin(400 * (t % 1)) : Math.sin(500 * (t % 4))); 
   var m = melody[Math.floor(t * 2 % melody.length)];
   
-  return ( 0.15 * sin(400 * m) + square(300 * m) + sin(f)) 
+  return (sin(400 * m) + square(300 * m) + sin(f)) 
       
   / 3 * (
     t % 5 > 2 
       ? (square(2) + square(6))
       : (sin(1) + square(3))
     ) / 2 + (t % 2 < 1/2 ? 
-            (t % 1/2 < 1/24 ? Math.random() : 0)
+            (t % 1/2 < 1/24 ? sin(550) : 0)
             : (t % 1/2 < 1/13 ? Math.random() : 0)); 
 
   function sin(freq) {
